c923038a4eadc41b250f65b157daaee6e7c56819
Sangram <sangram15.behera@gmail.com>
2022-04-05 05:25:32 +0530

    Fourth Commit

---
+++ multiplication.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+echo "Enter First Number"
+read First Number
+
+echo "Enter Second Number"
+read Second Number
+
+multiply=$(($First Number*Second Number))
+echo "the multiply is $multiply"
